6f3f836347d73c574d4e6f4b11274c891797f63d
Shiraz Ali <shirazkk8@gmail.com>
2024-07-26 18:33:18 +0500

    100DaysOfCodeChallenge

---
+++ Day-16/Q-46.ts
@@ -0,0 +1,25 @@
+//Question-46
+
+
+// type make_laptop={
+//     make:string,
+//     model:number,
+//     year:number
+// }
+ 
+const laptop={
+    make:"lenovo Thingpad T470",
+    model:12,
+    year:2015,
+    describe(){ 
+        console.log(`This laptop is a:${this.make} model:${this.model} year:${this.year}`);
+        
+        
+    }
+  
+}
+laptop.describe()
+
+ 
+
+
