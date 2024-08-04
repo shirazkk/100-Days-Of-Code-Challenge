c051179c9cd3622992052931beeb50b7ea495b0d
Shiraz Ali <shirazkk8@gmail.com>
2024-08-04 11:04:54 +0500

    Day25

---
+++ Day-25/Q-73.ts
@@ -0,0 +1,8 @@
+//Define a function which define how to assign and update value
+function variable() {
+  let a = "initial value"; //assign value
+  console.log(`Initial value:${a}`); //log the initial value
+  a = "update value"; //update the value
+  console.log(`Update value:${a}`); //log the updated value
+}
+variable();
---
+++ Day-25/Q-75.ts
@@ -0,0 +1,19 @@
+// Function to demonstrate compound assignment operations
+function demonstrateCompoundAssignments() {
+  let value = 5; // Initialize variable with 5
+  console.log(`Initial Value: ${value}`); // Log initial value
+
+  value += 2; // Add 2
+  console.log(`After Addition: ${value}`); // Output: 7
+
+  value *= 2; // Multiply by 2
+  console.log(`After Multiplication: ${value}`); // Output: 14
+
+  value -= 2; // Subtract 2
+  console.log(`After Subtraction: ${value}`); // Output: 12
+
+  value /= 2; // Divide by 2
+  console.log(`After Division: ${value}`); // Output: 6
+}
+
+demonstrateCompoundAssignments();
